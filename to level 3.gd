d3c47eaa9ca8668e871058b1a5b7c36d36b24510
Kyroro-sama <Kyroro-sama>
2024-07-26 14:08:22 +0200

    web

--- to level 3.gd
+++ to level 3.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
-@export var newScene = preload("res://level_3.tscn")
+@export var newScene = "main"
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
-		get_tree().change_scene_to_packed(newScene)
+		get_tree().change_scene_to_file(newScene + ".tscn")
